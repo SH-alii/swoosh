092c26f02f8f440f9b01173df308ee515f5e33fb
Alii Shareef <Alii@Aliis-MacBook-Pro.local>
2017-08-11 19:43:17 +0800

    Sending Data to VC

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,20 +10,81 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBOutlet weak var btnCoeds: BorderButton!
+    @IBOutlet weak var btnWomens: BorderButton!
+    @IBOutlet weak var btnMens: BorderButton!
+    @IBOutlet weak var btnNext: BorderButton!
+    
+    
+    var Selected = ""
+    
+    var player: Player!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
+   
+    @IBAction func onMensTapped(_ sender: Any){
+        selectLeague(leagueType: "Mens", button: btnMens, color: UIColor.blue.cgColor )
+    }
+    @IBAction func onWomensTapped(_ sender: Any){
+        selectLeague(leagueType: "Womens", button: btnWomens, color: UIColor.purple.cgColor )
+    }
+    @IBAction func onCoedsTapped(_ sender: Any){
+        selectLeague(leagueType: "Coe-eds", button: btnCoeds, color: UIColor.orange.cgColor)
+    }
+    
+    func selectLeague(leagueType: String, button: BorderButton, color: CGColor){
+        player.desiredLeague = leagueType
+        btnNext.isEnabled = true
+        if Selected != leagueType
+        {
+            Untoggle(selected: Selected)
+        }
+        Selected = leagueType
+        setColor(button: button, color: color)
+    }
+    
+    func Untoggle(selected: String){
+        
+        switch Selected {
+        case "Mens":
+            setColor(button: btnMens, color: UIColor.white.cgColor)
+        case "Womens":
+            setColor(button: btnWomens, color: UIColor.white.cgColor)
+        case "Coe-eds":
+            setColor(button: btnCoeds, color: UIColor.white.cgColor)
+        default:
+            break
+        }
+    }
+    
+    func setColor(button: BorderButton, color: CGColor)
+    {
+        button.layer.borderColor = color
+        
+    }
+    
     @IBAction func unwindFromLevelVC(segue: UIStoryboardSegue){
         
         
     }
     
-    
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            
+            
+        }
+    }
+    
+    
+    
+    
+    
     
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Alii Shareef on 8/11/17.
+//  Copyright © 2017 Alii Shareef. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
